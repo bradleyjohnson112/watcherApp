3e75e772b6cdcf5bfd00c4f80fdccdf199c21873
bradley <bradleyworkacc@outlook.com>
2022-11-08 17:19:20 +0000

    Added built in client side form validation for register and login views and added server side validation (char limit check and regex check on email input) to loginUser and registerUser methods

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -29,7 +29,17 @@ const registerUser = (req, res) => {
       confirm,
       err: "Password must match",
     });
+    // Check if email is incorrect format or exceeds char limit
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) || email.length > 254) {
+    res.render("register", {
+      username,
+      email,
+      password,
+      confirm,
+      err: "Email not accepted. Try again",
+    });
   } else {
+    // Check user with provided email doesn't already exist
     User.findOne({ email: email.toLowerCase() }).then((user) => {
       if (user) {
         res.render("register", {
@@ -39,33 +49,33 @@ const registerUser = (req, res) => {
           confirm,
           err: "User with that email already exists, try Logging in",
         });
-      }
+      } else {
+        // Create new user
+        const newUser = new User({
+          username,
+          email: email.toLowerCase(),
+          password,
+        });
 
-      // Create new user
-      const newUser = new User({
-        username,
-        email: email.toLowerCase(),
-        password,
-      });
-
-      // Hash password
-      bcrypt
-        .hash(newUser.password, 10)
-        .then((hash) => {
-          // Save user to db
-          newUser.password = hash;
-          return newUser.save();
-        })
-        .then(() => res.redirect("/login"))
-        .catch((err) =>
-          res.render("register", {
-            username,
-            email,
-            password,
-            confirm,
-            err: err,
+        // Hash password
+        bcrypt
+          .hash(newUser.password, 10)
+          .then((hash) => {
+            // Save user to db
+            newUser.password = hash;
+            return newUser.save();
           })
-        );
+          .then(() => res.redirect("/login"))
+          .catch((err) =>
+            res.render("register", {
+              username,
+              email,
+              password,
+              confirm,
+              err: err,
+            })
+          );
+      }
     });
   }
 };
@@ -85,6 +95,12 @@ const loginUser = (req, res) => {
       email,
       password,
       err: "Please fill in all the fields",
+    });
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) || email.length > 254) {
+    res.render("login", {
+      email,
+      password,
+      err: "Email not accepted. Try again",
     });
   } else {
     passport.authenticate("local", {
